af1a48ca71451466dc255e1c713acf69acc24cef
tadree <P@ssword1408>
2023-01-16 15:23:56 +0300

    Formatted code

--- src/main/java/ru/jiehk/PracticeWithOperatorsAndDataTypes.java
+++ src/main/java/ru/jiehk/PracticeWithOperatorsAndDataTypes.java
@@ -8,34 +8,34 @@ public class PracticeWithOperatorsAndDataTypes {
         System.out.println("Диапазон для типа double: " + Double.MIN_VALUE + " - " + Double.MAX_VALUE + "\n");
 
         byte c = 125;
-        for(int i=1;i<=5;i++){
+        for (int i = 1; i <= 5; i++) {
             c++;
             System.out.println(c);
         }
 
         int a = 127;
         double b = 1.5;
-        System.out.println((a+b));
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
         System.out.println(++b);
         System.out.println(a++);
 
-        for(int i=1;i<=15;i++){
-            if(i%2 == 0) {
+        for (int i = 1; i <= 15; i++) {
+            if (i % 2 == 0) {
                 System.out.println(i + " - четное число");
             }
         }
         int d = 22;
-        System.out.println(a>d);
-        System.out.println(a<d);
-        System.out.println(a<=d);
-        System.out.println(a>=d);
-        System.out.println(a!=d);
-        System.out.println(a==d);
-        System.out.println(a>d && b == 2.5);
-        System.out.println(!(a>d && b == 2.5));
+        System.out.println(a > d);
+        System.out.println(a < d);
+        System.out.println(a <= d);
+        System.out.println(a >= d);
+        System.out.println(a != d);
+        System.out.println(a == d);
+        System.out.println(a > d && b == 2.5);
+        System.out.println(!(a > d && b == 2.5));
     }
 }
